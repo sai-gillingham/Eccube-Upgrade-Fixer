8e520ab11db75237d4d8dc9592d3f83753f821c3
kiy0taka <kiy0taka333@gmail.com>
2017-02-28 10:50:06 +0900

    fixed typo

--- src/Symfony/Upgrade/Fixer.php
+++ src/Symfony/Upgrade/Fixer.php
@@ -12,7 +12,7 @@ use Symfony\Upgrade\Fixer\Iterator\FixerIterator;
 
 class Fixer
 {
-    const VERSION = '0.1.5-eccube-3.1.0-aplpha';
+    const VERSION = '0.1.5-eccube-3.1.0-alpha';
 
     private $fixers = [];
     private $finder;
