461deb76fd42e4844e2cd5bef64a4ae32e0d63fe
ギリングハムピアース <gillingham@sai-net.jp>
2022-05-30 11:58:39 +0900

    Swift mailer plugin check update.

--- src/Symfony/Upgrade/Fixer/SwiftMailerChangeFixer.php
+++ src/Symfony/Upgrade/Fixer/SwiftMailerChangeFixer.php
@@ -8,6 +8,7 @@ use UnexpectedValueException;
 
 class SwiftMailerChangeFixer extends AbstractFixer
 {
+    public bool $didUpdateMailer = false;
 
     /**
      * @inheritDoc
@@ -15,27 +16,16 @@ class SwiftMailerChangeFixer extends AbstractFixer
     public function fix(\SplFileInfo $file, $content)
     {
         $tokens = Tokens::fromCode($content);
-        if ($file->getFilename() == 'MailMagazineService.php') {
+        $this->didUpdateMailer = false;
+        $this->updateEmailClass($tokens);
+        if ($this->didUpdateMailer === true) {
             // Useステートメントの追加
             // Add Use Statements
             $this->addUseStatement($tokens, ['Symfony', 'Component', 'Mailer', 'MailerInterface']);
             $this->addUseStatement($tokens, ['Symfony', 'Component', 'Mime', 'Address']);
             $this->addUseStatement($tokens, ['Symfony', 'Component', 'Mime', 'Email']);
-
             // Swift_MessageクラスをEmailクラスに変更する
             // Change Swift_Message class to Email Class
-            $swiftMessageClass = $tokens->findSequence([
-                [T_NEW],
-                [T_NS_SEPARATOR],
-                [T_STRING, 'Swift_Message'],
-                '(',
-                ')'
-            ]);
-            if ($swiftMessageClass) {
-                $useTokenIndexes = array_keys($swiftMessageClass);
-                $tokens[end($useTokenIndexes) - 3]->setContent('');
-                $tokens[end($useTokenIndexes) - 2]->setContent('Email');
-            }
             // 配列のfrom型をAddressクラスのインスタンスに置き換える
             // Replace "from" array type with Address class instance
             $this->_replaceArrayWithAddressObject($tokens);
@@ -44,10 +34,14 @@ class SwiftMailerChangeFixer extends AbstractFixer
             $this->_replaceChainFunction($tokens, 'setSubject', 'subject');
             $this->_replaceChainFunction($tokens, 'setFrom', 'from');
             $this->_replaceChainFunction($tokens, 'setTo', 'to');
+            $this->_replaceChainFunction($tokens, 'setBcc', 'bcc');
             $this->_replaceChainFunction($tokens, 'setReplyTo', 'replyTo');
             $this->_replaceChainFunction($tokens, 'setReturnPath', 'returnPath');
             $this->_replaceChainFunction($tokens, 'setBody', 'text');
             $this->_replaceChainFunction($tokens, 'addPart', 'html');
+            // メール
+            $this->_deleteChainFunction($tokens, 'setContentType');
+
             // Swift_Messageの型への参照をすべてMailInterfaceに置き換える
             // Replace any reference to type of Swift_Message to MailInterface
             $this->_parameterTypeFix($tokens);
@@ -55,66 +49,133 @@ class SwiftMailerChangeFixer extends AbstractFixer
         return $tokens->generateCode();
     }
 
+    private function updateEmailClass(Tokens $tokens, int $index = 0)
+    {
+        $swiftMessageClass = $tokens->findSequence(
+            [
+                [T_NEW],
+                [T_NS_SEPARATOR],
+                [T_STRING, 'Swift_Message'],
+                '(',
+                ')'
+            ],
+            $index
+        );
+        if ($swiftMessageClass == null) {
+            return;
+        }
+        $useTokenIndexes = array_keys($swiftMessageClass);
+        $tokens[end($useTokenIndexes) - 3]->setContent('');
+        $tokens[end($useTokenIndexes) - 2]->setContent('Email');
+        $this->didUpdateMailer = true;
+        $this->updateEmailClass($tokens, end($useTokenIndexes));
+    }
 
-    private function _replaceChainFunction(Tokens &$tokens, string $from, string $to)
+
+    private function _replaceChainFunction(Tokens &$tokens, string $from, string $to, $index = 0)
     {
         // Swift_Message クラスを変更する
         // Change Swift_Message class
-        $subjectFunctionUpdate = $tokens->findSequence([
-            [T_OBJECT_OPERATOR],
-            [T_STRING, $from],
-        ]);
-        if ($subjectFunctionUpdate) {
-            $useTokenIndexes = array_keys($subjectFunctionUpdate);
-            $tokens[end($useTokenIndexes)]->setContent($to);
+        $subjectFunctionUpdate = $tokens->findSequence(
+            [
+                [T_OBJECT_OPERATOR],
+                [T_STRING, $from],
+            ],
+            $index);
+
+        if ($subjectFunctionUpdate == null) {
+            return;
         }
+
+        $useTokenIndexes = array_keys($subjectFunctionUpdate);
+        $tokens[end($useTokenIndexes)]->setContent($to);
+        $this->_replaceChainFunction($tokens, $from, $to, end($useTokenIndexes));
     }
 
-    private function _replaceArrayWithAddressObject(Tokens &$tokens)
+    /**
+     * @param Tokens $tokens
+     * @param string $from
+     * @param int $index
+     * @return void
+     */
+    private function _deleteChainFunction(Tokens &$tokens, string $from, int $index = 0)
     {
-        $arrayStart = $tokens->findSequence([
-            [T_OBJECT_OPERATOR],
-            [T_STRING, 'setFrom'],
-            '(',
-            '['
-        ]);
+        $deleteChainFunction = $tokens->findSequence(
+            [
+                [T_OBJECT_OPERATOR],
+                [T_STRING, $from],
+                '('
+            ],
+            $index
+        );
 
-        if ($arrayStart) {
-            $arrayStartIndex = array_keys($arrayStart);
-            $endToken = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_SQUARE_BRACE, end($arrayStartIndex));
-            $startTokenIndex = end($arrayStartIndex);
-            $email = [];
-            $emailName = [];
-            $isNameNow = false;
-            $tokens[$endToken]->setContent('');
-            for ($y = $startTokenIndex; $y <= $endToken + 1; $y++) {
-                /** @var $tokens Token[] */
-                if ($tokens[$y]->equals([T_DOUBLE_ARROW, '=>'])) {
-                    $isNameNow = true;
-                    $tokens[$y]->setContent('');
-                    continue;
-                }
-                if ($isNameNow) {
-                    $emailName[] = $tokens[$y]->getContent();
-                } else {
-                    $email[] = $tokens[$y]->getContent();
-                };
-                $tokens[$y]->setContent('');
-            }
-            array_shift($email);
-            $emailString = implode('', $email);
-            $emailName = implode('', $emailName);
-            $tokens->insertAt($startTokenIndex, [
-                new Token([T_NEW, 'new']),
-                new Token([T_WHITESPACE, ' ']),
-                new Token([T_STRING, 'Address']),
-                new Token([T_STRING, '(']),
-                new Token([T_STRING, $emailString]),
-                new Token([T_STRING, ',']),
-                new Token([T_STRING, $emailName]),
-                new Token([T_STRING, ')'])
-            ]);
+        if ($deleteChainFunction == null) {
+            return;
         }
+
+        $deleteFunctionToken = array_keys($deleteChainFunction);
+        var_dump($deleteFunctionToken);
+
+        $methodEndToken = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, end($deleteFunctionToken));
+
+        for ($i = $deleteFunctionToken[0]; $i < $methodEndToken + 1; $i++) {
+            /** @var Tokens|Token[] $tokens */
+            $tokens[$i]->setContent('');
+        }
+        $this->_deleteChainFunction($tokens, $from, $methodEndToken);
+    }
+
+    private function _replaceArrayWithAddressObject(Tokens &$tokens, int $index = 0)
+    {
+        $arrayStart = $tokens->findSequence(
+            [
+                [T_OBJECT_OPERATOR],
+                [T_STRING, 'setFrom'],
+                '(',
+                '['
+            ],
+            $index
+        );
+
+        if ($arrayStart == null) {
+            return;
+        }
+
+        $arrayStartIndex = array_keys($arrayStart);
+        $endToken = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_SQUARE_BRACE, end($arrayStartIndex));
+        $startTokenIndex = end($arrayStartIndex);
+        $email = [];
+        $emailName = [];
+        $isNameNow = false;
+        $tokens[$endToken]->setContent('');
+        for ($y = $startTokenIndex; $y <= $endToken + 1; $y++) {
+            /** @var $tokens Token[] */
+            if ($tokens[$y]->equals([T_DOUBLE_ARROW, '=>'])) {
+                $isNameNow = true;
+                $tokens[$y]->setContent('');
+                continue;
+            }
+            if ($isNameNow) {
+                $emailName[] = $tokens[$y]->getContent();
+            } else {
+                $email[] = $tokens[$y]->getContent();
+            };
+            $tokens[$y]->setContent('');
+        }
+        array_shift($email);
+        $emailString = implode('', $email);
+        $emailName = implode('', $emailName);
+        $tokens->insertAt($startTokenIndex, [
+            new Token([T_NEW, 'new']),
+            new Token([T_WHITESPACE, ' ']),
+            new Token([T_STRING, 'Address']),
+            new Token([T_STRING, '(']),
+            new Token([T_STRING, $emailString]),
+            new Token([T_STRING, ',']),
+            new Token([T_STRING, $emailName]),
+            new Token([T_STRING, ')'])
+        ]);
+        $this->_replaceArrayWithAddressObject($tokens, $endToken);
     }
 
     private function _parameterTypeFix(Tokens &$tokens, int $parameterIndex = 0)
@@ -135,17 +196,17 @@ class SwiftMailerChangeFixer extends AbstractFixer
             $endToken = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, end($arrayStartIndex));
         } catch (UnexpectedValueException $exception) {
             echo "NO END";
-            $this->_parameterTypeFix($tokens, end($arrayStartIndex) + 1);
         }
 
-        for ($y = end($arrayStartIndex); $y < $endToken; $y++) {
-            /** @var Token[] $tokens */
-            if ($tokens[$y]->getContent() == 'Swift_Mailer') {
-                $tokens[$y - 1]->setContent('');
-                $tokens[$y]->setContent('MailerInterface');
+        if (isset($endToken)) {
+            for ($y = end($arrayStartIndex); $y < $endToken; $y++) {
+                /** @var Token[] $tokens */
+                if ($tokens[$y]->getContent() == 'Swift_Mailer') {
+                    $tokens[$y - 1]->setContent('');
+                    $tokens[$y]->setContent('MailerInterface');
+                }
             }
         }
-
         $this->_parameterTypeFix($tokens, end($arrayStartIndex) + 1);
     }
 
