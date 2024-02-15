cce4af6d936a238d8cb18efd4738f610f41dd5a6
harauchi <harauchi@sai-net.jp>
2024-02-15 18:29:01 +0900

    レポジトリ取得に関するFixer作成

---
+++ src/Symfony/Upgrade/Fixer/GetRepositoryFixer.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace Symfony\Upgrade\Fixer;
+
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+use Symfony\Upgrade\Util\UseTokenUtil;
+
+class GetRepositoryFixer extends AbstractFixer
+{
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isTokenKindFound(T_DOC_COMMENT);
+    }
+
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition(
+            'Converts simple usages of `array_push($x, $y);` to `$x[] = $y;`.',
+            [new CodeSample("<?php\narray_push(\$x, \$y);\n")],
+            null,
+            'Risky when the function `array_push` is overridden.'
+        );
+    }
+
+    public function applyFix(\SplFileInfo $file, Tokens $tokens): void
+    {
+        // コンテナを使っていないなら判定する必要は無い
+        $fqcn1 = ['Symfony','Component', 'DependencyInjection', 'ContainerInterface'];
+        $fqcn2 = ['Psr','Container', 'ContainerInterface'];
+        if(!$this->hasUseStatements($tokens, $fqcn1) && !$this->hasUseStatements($tokens, $fqcn2)){
+            return;
+        }
+
+        // ファイル内の全トークンを判定する
+        foreach($tokens as $key => $token){
+            if($token->getContent() == "get"){
+                // 引数の判定を実行
+                if(str_contains($RepositoryClass = $tokens[$key +2]->getContent(), "\Repository\\")){
+                    // get(レポジトリクラス指定);
+                    // get('doctrine.orm.entity_manager')->getRepository(エンティティクラス::class);
+                    
+                    // **Repositoryの文字列を取得
+                    $position = strrpos($RepositoryClass, '\\');
+                    $ClassName = substr($RepositoryClass, $position + 1);
+                    // Repositoryを排除
+                    $ClassName = substr($ClassName, 0, -11). '::class';
+
+                    $newContent1 = new Token([T_STRING, '"doctrine.orm.entity_manager"']);
+
+                    $tokens[$key + 2] = $newContent1;
+
+                    $tokens->insertAt(
+                        $key + 4,
+                        array_merge(
+                            [
+                                new Token([T_OBJECT_OPERATOR, '->']),
+                                new Token([T_STRING, 'getRepository']),
+                                new Token([T_STRING, '(']),
+                                new Token([T_CLASS, $ClassName]),
+                                new Token([T_STRING, ')']),
+                            ]
+                        )
+                    );
+                }
+            }
+        }
+    }
+
+    public function getDescription()
+    {
+        return 'Fix ServiceProvider.';
+    }
+}
